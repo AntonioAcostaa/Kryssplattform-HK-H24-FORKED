8c5bba4451c42f8daca86069ff9f535b3489ca36
Antonio Acosta <antonio.acosta@intility.no>
2024-09-20 11:47:56 +0200

    Forelesning 5

--- app/(app)/postDetails/[id].tsx
+++ app/(app)/postDetails/[id].tsx
@@ -4,6 +4,7 @@ import { PostData } from "@/utils/postData";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
 import { Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function postDetails() {
   const { id } = useLocalSearchParams();
@@ -32,6 +33,28 @@ export default function postDetails() {
         }}
       />
       <Text>{post?.title}</Text>
+      <View style={{width: "100%", maxHeight: "50%", flex: 1}}>
+        {post?.postCoordinates && (
+            <MapView
+                style={{width: "100%", height: "100%"}}
+                initialRegion={{
+                latitude: post.postCoordinates.latitude,
+                longitude: post.postCoordinates.longitude,
+                latitudeDelta: 0.0022,
+                longitudeDelta: 0.0022,
+                }}
+            >
+                <Marker
+                coordinate={{
+                    latitude: post.postCoordinates.latitude,
+                    longitude: post.postCoordinates.longitude,
+                }}
+                title={post.title}
+                description={post.description}
+                />
+                </MapView>
+        )}
+      </View>
     </View>
   );
 }
